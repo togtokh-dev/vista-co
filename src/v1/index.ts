2ea0bb4a6a07ed003696960530a9a71ec2daea23
buyantogtokh.alt <buyantogtokh.alt@unitel.mn>
2025-01-13 17:24:35 +0800

    main

--- src/v1/index.ts
+++ src/v1/index.ts
@@ -2,13 +2,16 @@ import OData from "./OData";
 import BackgroundJob from "./BackgroundJob";
 import Booking from "./Booking";
 import RESTData from "./RESTData";
+import RESTTicketing from "./RESTTicketing";
 export default {
   BackgroundJob,
   Booking,
   RESTData,
+  RESTTicketing,
   OData,
 };
 export { default as BackgroundJob } from "./BackgroundJob";
 export { default as Booking } from "./Booking";
 export { default as OData } from "./OData";
 export { default as RESTData } from "./RESTData";
+export { default as RESTTicketing } from "./RESTTicketing";
